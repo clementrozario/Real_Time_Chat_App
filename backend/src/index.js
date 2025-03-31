d0ded1a5568c223da5e20505dc6e03ea4c4026ce
Clement Delphin <clementcla@outlook.com>
2025-03-31 19:07:40 +0530

    message routes for backend implemented not socket io

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,9 @@
 import express from "express";
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser";
-import authRoutes from "../src/routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
+import authRoutes from "../src/routes/auth.route.js";
+import mesaageRoutes from "../src/routes/message.route.js";
 
 dotenv.config();
 
@@ -13,7 +14,8 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/auth",authRoutes)
+app.use("/api/auth",authRoutes);
+app.use("api/message",mesaageRoutes);
 
 
 app.listen(PORT, () => {
