703bb1c410272cc26469ab1ff2a9c707913a1350
Clement Delphin <clementcla@outlook.com>
2025-03-31 16:48:39 +0530

    implemented auth routes for profile pic

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,3 +1,5 @@
+import { error } from "console";
+import cloudinary from "../lib/cloudinary.js";
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs"
@@ -72,23 +74,43 @@ export const login = async (req,res) => {
 
 export const logout = (req,res) => {
     try{
-        res.cookie("jwt","",{maxAge:0})
-        res.status(200).json({message:"cle successfully"})
+        res.cookie("jwt","",{maxAge:0});
+        res.status(200).json({message:"Logged out successfully"});
     }catch(error){
-        console.log("error",error.message);
+        console.log("Error in logout controller",error.message);
+        res.status(500).json({message:"Internal server Error"});
     }
 };
 
+export const updateProfile = async(req,res) =>{
+    try{
+        const {profilepic} = req.body;
+        const userId = req.user._id;
+
+        if(!profilepic){
+            return res.status(400).json({message:"profile pic is required"});
+        }
+
+        const uploadResponse = await cloudinary.uploader.upload(profilepic)
+        const updatedUser = await User.findByIdAndUpdate(userId,{profilepic:uploadResponse.secure_url},{new:true})
+
+        res.status(200).json(updatedUser)
+    }catch{
+        console.log("error in update profile:",error)
+        res.status(500).json({message:"Internal server Error"})
+    }
+}
+    export const checkAuth = (req,res) =>{
+        try{
+            res.status(200).json(req.user);
+        }catch(error){
+            console.log("Error in checkAuth controller",error.message);
+            res.status(500).json({message:"Internal server Error"});
+        }
+    }
 
 
 
 
 
 
-// try{
-//     res.cookie("jwt","",{maxAge:0});
-//     res.status(200).json({message:"Logged out successfully"});
-// }catch(error){
-//     console.log("Error in logout controller",error.message);
-//     res.status(500).json({message:"Internal server Error"});
-// }
