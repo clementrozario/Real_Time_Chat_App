80ac6287e07e20ab4a70d789d0a25188d846a0fd
Clement Delphin <clementcla@outlook.com>
2025-04-03 11:24:04 +0530

    implemented signup,login pages with zustand,toast&styles

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,5 +1,7 @@
 import { create } from "zustand";
 import { axiosInstance } from "../lib/axios";
+import toast from "react-hot-toast";
+
 
 export const useAuthStore = create((set) => ({
     authUser:null,
@@ -19,9 +21,44 @@ export const useAuthStore = create((set) => ({
         }finally{
             set({isCheckingAuth:false})
         }
-    }
+    },
 
-    signup:async (data) => {
-        
-    }
+    signup: async (data) => {
+        set({ isSigningUp: true });
+        try {
+          const res = await axiosInstance.post("/auth/signup", data);
+          set({ authUser: res.data });
+          toast.success("Account created successfully");
+          
+        } catch (error) {
+          toast.error(error.response.data.message);
+        } finally {
+          set({ isSigningUp: false });
+        }
+      },
+      
+      login: async (data) => {
+        set({ isLoggingIn: true });
+        try {
+          const res = await axiosInstance.post("/auth/login", data);
+          set({ authUser: res.data });
+          toast.success("Logged in successfully");
+
+        } catch (error) {
+          toast.error(error.response.data.message);
+        } finally {
+          set({ isLoggingIn: false });
+        }
+      },
+    
+      logout: async () => {
+        try {
+          await axiosInstance.post("/auth/logout");
+          set({ authUser: null });
+          toast.success("Logged out successfully");
+         
+        } catch (error) {
+          toast.error(error.response.data.message);
+        }
+      },
 }))
